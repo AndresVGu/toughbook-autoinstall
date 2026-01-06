1cba1f6cb83790049439d7de23df98d25a51d9cb
Andres V. <146892430+AndresVGu@users.noreply.github.com>
2026-01-06 14:59:37 -0500

    Update touch-calibrator-cf31.sh

--- touch-calibrator-cf31.sh
+++ touch-calibrator-cf31.sh
@@ -7,12 +7,12 @@ DEVICE="Fujitsu Component USB Touch Panel"
 #-------Escala----------
 
 SCALE_X=1.10     # >1 expande, <1 encoge en X
-SCALE_Y=1.10     # >1 expande, <1 encoge en Y 
+SCALE_Y=1.15     # >1 expande, <1 encoge en Y 
 
 #---------Offset----------
 
-OFFSET_X=-0.042    #Negativo = mueve a la izquierda
-OFFSET_Y=-0.07    #Negativo = mueve hacia arriba 
+OFFSET_X=-0.045    #Negativo = mueve a la izquierda
+OFFSET_Y=-0.085    #Negativo = mueve hacia arriba 
 
 MATRIX="$SCALE_X 0 $OFFSET_X 0 $SCALE_Y $OFFSET_Y 0 0 1"
 
