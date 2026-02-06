553e93e25d6cf32aecbc8b06f9e2d8f44222ed2c
Andres V. <146892430+AndresVGu@users.noreply.github.com>
2026-02-06 12:38:31 -0500

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -208,8 +208,8 @@ check_neofetch() {
     
 
     #brand & model
-    brand=$(sudo dmidecode -s system-manufacturer | awk '{print $1}' 2>/dev/null)
-    model=$(sudo dmidecode -s system-product-name | sed -r 's/([A-Z]{2})([0-9]{2})-([0-9])/\1-\2 MK\3/' 2>/dev/null)
+    #brand=$(sudo dmidecode -s system-manufacturer | awk '{print $1}' 2>/dev/null)
+  #  model=$(sudo dmidecode -s system-product-name | sed -r 's/([A-Z]{2})([0-9]{2})-([0-9])/\1-\2 MK\3/' 2>/dev/null)
 
 	#serial & part number
    # serial=$(sudo dmidecode -s system-serial-number 2>/dev/null)
@@ -218,28 +218,28 @@ check_neofetch() {
    # cpu=$(lscpu | grep "BIOS Model name:" | sed 's/BIOS Model name:\s*//')
 
 	# --- Bloque de Validación y Modificación de la variable 'brand' ---
-	case "$model" in
+	#case "$model" in
     	# Si la salida es exactamente CF-54-2
-    	"CF-54-2")
-        	model="CF-54 Mk2"
-			cpu=$(lscpu | grep "BIOS Model name:" | sed 's/BIOS Model name:\s*//')
+    	#"CF-54-2")
+        #	model="CF-54 Mk2"
+		#	cpu=$(lscpu | grep "BIOS Model name:" | sed 's/BIOS Model name:\s*//')
         	;;
     	# Si la salida es g1-1a (la validación es sensible a mayúsculas y minúsculas por defecto)
-    	"FZ-G1A"*)
-        	model="FZ-G1 MK1"
+    	#"FZ-G1A"*)
+        #	model="FZ-G1 MK1"
 			#part_number=$(sudo dmidecode -s system-product-name | sed -r 's/([A-Z]{2})([0-9]{2})-([0-9])/\1-\2 MK\3/' 2>/dev/null)
-			cpu=$(lscpu | grep "Model name:" | sed 's/Model name:\s*//')
-        	;;
+		#	cpu=$(lscpu | grep "Model name:" | sed 's/Model name:\s*//')
+        #	;;
     	# Caso por defecto (*): si no coincide con ninguno de los anteriores,
     	# no se ejecuta nada, y la variable 'brand' mantiene su valor original.
-		"CF-53 MK4")
-			cpu=$(lscpu | grep "Model name:" | sed 's/Model name:\s*//')
-			;;
-    	*)
-			cpu=$(lscpu | grep "BIOS Model name:" | sed 's/BIOS Model name:\s*//')
+		#"CF-53 MK4")
+		#	cpu=$(lscpu | grep "Model name:" | sed 's/Model name:\s*//')
+		#	;;
+    	#*)
+		#	cpu=$(lscpu | grep "BIOS Model name:" | sed 's/BIOS Model name:\s*//')
         	# Opcional: puedes añadir un 'echo' para debug aquí si quieres
-        	;;
-	esac
+        #	;;
+	#esac
 	# -----------------------------------------------------------------
 
     #hours
